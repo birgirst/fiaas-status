69916a16ceee69a1c55e0107c2cfc117b6136688
birgirst <birgir@gmail.com>
2021-12-03 09:46:02 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 
 interval=$1
 checks=$2
+deployment_id=$3
 
 # Extract the base64 encoded config data and write this to the KUBECONFIG
 echo "$KUBE_CONFIG_DATA" | base64 -d > /tmp/config
@@ -12,7 +13,7 @@ i=0
 while [ $i -lt $checks ]
 do
   echo "checking status"
-  STATUS=$(sh -c "kubectl get applicationstatus -lfiaas/deployment_id=$* -ojson | jq '.items | .[] | .result' | sed 's/\\\"//g'")
+  STATUS=$(sh -c "kubectl get applicationstatus -lfiaas/deployment_id=$deployment_id -ojson | jq '.items | .[] | .result' | sed 's/\\\"//g'")
   if [ "SUCCESS" = "$STATUS" ]; then
     echo "success"
     echo "::set-output name=kubectl-output::$STATUS"
